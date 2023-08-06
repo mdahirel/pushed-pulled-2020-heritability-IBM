dcf5605e93f7540c076ce3a6dbca407d5cc31094
Maxime Dahirel <maxime.dahirel@yahoo.fr>
2023-08-06 16:44:21 +0200

    Update test_parameter_space_alpha_beta.R

--- R/test_parameter_space_alpha_beta.R
+++ R/test_parameter_space_alpha_beta.R
@@ -2,18 +2,6 @@ alpha <- c(-6,-4,-2,0,2,4,6)
 beta <- c(0,0.25,0.5,0.75,1)
 dmax <- 0.5
 
-expand_grid(alpha,beta,dmax) |> 
-  mutate(d0 = dmax/(1+exp(-alpha * (0 - beta))),
-         dK = dmax/(1+exp(-alpha * (1 - beta)))
-  ) |> 
-  ggplot() + 
-  geom_contour_filled(aes(x=alpha,y=beta,z=(dK-d0)))
-
-
-
-
-
-
 parsamples <- expand_grid(alpha,beta,dmax) |> 
   expand_grid(
     tibble(zscores_a=rnorm(1000,0,1),
@@ -26,57 +14,54 @@ parsamples <- expand_grid(alpha,beta,dmax) |>
 ## version with phenotypic var in DDD
 
 parsamples |> 
-  mutate(d0 = dmax/(1+exp(-alpha_sample * (0 - beta_sample))),
-         dK = dmax/(1+exp(-alpha_sample * (1 - beta_sample)))
+  mutate(
+    d0 = dmax/(1+exp(-alpha * (0 - beta))),
+    dK = dmax/(1+exp(-alpha * (1 - beta))),
+    d0_sample = dmax/(1+exp(-alpha_sample * (0 - beta_sample))),
+    dK_sample = dmax/(1+exp(-alpha_sample * (1 - beta_sample)))
   ) |> 
-  mutate(delta = dK-d0) |> 
+  mutate(delta_sample = dK_sample - d0_sample,
+         delta = dK - d0) |> 
   group_by(alpha,beta,dmax) |> 
-  summarise(meandelta=mean(delta)) |> 
+  summarise(delta=mean(delta), meandelta_sample = mean(delta_sample),
+            d0=mean(d0),meand0_sample = mean(d0_sample)) |> 
   ggplot() + 
-  geom_contour_filled(aes(x=alpha,y=beta,z=meandelta))
+  geom_contour_filled(aes(x=alpha,y=beta,z=meandelta_sample))
 
-## version with all phenotypic var
 
 parsamples |> 
-  mutate(d0 = dmax_sample/(1+exp(-alpha_sample * (0 - beta_sample))),
-         dK = dmax_sample/(1+exp(-alpha_sample * (1 - beta_sample)))
+  mutate(
+    d0 = dmax/(1+exp(-alpha * (0 - beta))),
+    dK = dmax/(1+exp(-alpha * (1 - beta))),
+    d0_sample = dmax/(1+exp(-alpha_sample * (0 - beta_sample))),
+    dK_sample = dmax/(1+exp(-alpha_sample * (1 - beta_sample)))
   ) |> 
-  mutate(delta = dK-d0) |> 
+  mutate(delta_sample = dK_sample - d0_sample,
+         delta = dK - d0) |> 
+  mutate(i=1:35000) |> 
+  group_by(i) |> 
+  nest() |> 
+  mutate(vstar = map(
+    .x=data,
+    .f = \(x) vstar(
+      fec=1.5,
+      disp=x$d0,
+      mort=0)
+  )
+  )|> 
+  mutate(vstar_sample = map(
+    .x=data,
+    .f = \(x) vstar(
+      fec=1.5,
+      disp=x$d0_sample,
+      mort=0)
+  )
+  )|> 
+  unnest(data) |> 
+  unnest(vstar) |> 
+  unnest(vstar_sample) |> 
   group_by(alpha,beta,dmax) |> 
-  summarise(meandelta=mean(delta)) |> 
-  ggplot() + 
-  geom_contour_filled(aes(x=alpha,y=beta,z=meandelta))
-
-## version with only phenotypic var in dmax
-
-parsamples |> 
-  mutate(d0 = dmax_sample/(1+exp(-alpha * (0 - beta))),
-         dK = dmax_sample/(1+exp(-alpha * (1 - beta)))
-  ) |> 
-  mutate(delta = dK-d0) |> 
-  group_by(alpha,beta,dmax) |> 
-  summarise(meandelta=mean(delta)) |> 
-  ggplot() + 
-  geom_contour_filled(aes(x=alpha,y=beta,z=meandelta))
-
-
-
-
-## version with all phenotypic var
-
-parsamples |> 
-  mutate(d0 = dmax_sample/(1+exp(-alpha_sample * (0 - beta_sample))),
-         dK = dmax_sample/(1+exp(-alpha_sample * (1 - beta_sample)))
-  ) |> 
-  mutate(delta = dK-d0) |> 
-  group_by(alpha,beta,dmax) |> 
-  summarise(meandelta=mean(delta)) |> 
-  mutate(fix_delta = 0.5/(1+exp(-alpha * (1 - beta))) - 
-           0.5/(1+exp(-alpha * (0 - beta)))) |> 
-  ggplot() + 
-  geom_point(aes(x=fix_delta,y=meandelta))+
-  lims(x=c(-0.5,0.5),y=c(-0.5,0.5))
-
-#including var in alpha and beta reduces the range of var in DDD, through weird averaging stuff
-
-
+  summarise(vstar=mean(vstar), meanvstar_sample = median(vstar_sample)) |>  
+  ggplot()+geom_point(aes(vstar,meanvstar_sample))+
+  geom_abline(intercept=0,slope=1)
+  
